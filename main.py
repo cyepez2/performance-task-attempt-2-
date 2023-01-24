b3dcc2c3f1d2da3a250baf6864aa01e04f232a6b
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2023-01-24 18:56:37 +0000

    made a little bit of progress

---
+++ main.py
@@ -0,0 +1,17 @@
+#import math
+
+
+
+"apple pie" == 9
+
+"red apple" == .50
+
+"fresh milk" == 1
+
+"cupcake" == 3
+
+
+print("You notice a small cottage with a sign posted in front reading KITTY'S BAKESHOP. You walk in and are greeted by Kitty, and she presents you the menu.")
+
+
+
---
+++ order.py
@@ -0,0 +1,14 @@
+#def order(order1):
+  
+menu = ["apple pie" , "red apple", "fresh milk", "cupcake"]
+  
+order1 = input("Hello this is Kitty, welcome to my bakery! Today you can choose from an apple pie, a red apple, some fresh milk, or a cupcake. What would you like?")
+
+if order1 in menu:
+
+    print("Okay! I can get you that!")
+
+else:
+    print("I unfortunatley do not serve that")
+    
+
